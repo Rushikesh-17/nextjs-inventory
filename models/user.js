f2ba5a4beb6da539312912a853c2fc315f7d0586
Rushikesh-17 <90689828+Rushikesh-17@users.noreply.github.com>
2023-01-17 19:16:22 +0530

    Delete models directory

--- models/user.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const UserSchema = new mongoose.Schema({
-    name: { type: String, required: true},
-    email: { type: String, required: true},
-    password: {type: String, required : true}
-});
-export default mongoose.model("user",UserSchema)
